f42a5f43f625e7f065fffebedc2c3b1d4ad2f0d4
Song Cho <bravochos@gmail.com>
2022-05-19 20:06:31 +0900

    add note component

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { PageComponent } from "./components/page/page.js";
 
@@ -12,6 +13,9 @@ class App {
       "https://picsum.photos/600/300"
     );
     image.attachTo(appRoot, "beforeend");
+
+    const note = new NoteComponent("Note Title", "Note Body");
+    note.attachTo(appRoot, "beforeend");
   }
 }
 
