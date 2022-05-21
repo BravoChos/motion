1ad7bb4ea10305481896aef91e5907f14a78323d
Song Cho <bravochos@gmail.com>
2022-05-21 18:20:00 +0900

    add video component

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { VideoComponent } from "./components/page/item/video.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
@@ -20,6 +21,12 @@ class App {
 
     const todo = new TodoComponent("Todo Title", "Todo Item");
     todo.attachTo(appRoot, "beforeend");
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://youtu.be/K3-jG52XwuQ"
+    );
+    video.attachTo(appRoot, "beforeend");
   }
 }
 
